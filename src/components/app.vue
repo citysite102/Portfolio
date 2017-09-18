e463c4d15da763747ae90b4ce23891d215c0cbbe
Samuel <citysite1025@gmail.com>
2017-09-18 20:08:13 +0800

    Update first section layout

--- src/components/app.vue
+++ src/components/app.vue
@@ -2,24 +2,35 @@
   <div>
     <headerBar></headerBar>
     <section class="section_about">
-      <div id="info"></div>
-        <h2>
-          Hi, I'm Samuel.
-        </h2>
-        <h3>
-          Software developer,
-        </h3>
-        <h3>
-          UI Designer,
-        </h3>
-        <h3>
-          Mentor in Taiwan
-        </h3>
-      <div id="picture"></div>
+      <el-row>
+        <el-col :span="10" :offset="2">
+          <div id="header-info">
+            <h2 id="header-name">
+              Hi, I'm Samuel.
+            </h2>
+            <h3 id="header-software">
+              Software developer,
+            </h3>
+            <h3 id="header-ui">
+              UI Designer,
+            </h3>
+            <h3 id="header-mentor">
+              Mentor <span>in Taiwan</span>
+            </h3>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div id="header-picture">
+            <h3 id="header-location">
+              2017_<br>Sanfrancisco
+            </h3>
+          </div>
+        </el-col>
+      </el-row>
     </section>
-    <el-button>默认按钮</el-button>
+    <!-- <el-button>默认按钮</el-button>
     <el-button type="primary">主要按钮</el-button>
-    <el-button type="text">文字按钮</el-button>
+    <el-button type="text">文字按钮</el-button> -->
   </div>
 </template>
 
@@ -42,6 +53,41 @@
   @import '~styles/main.sass'
   .section_about
     background-color: $default-background-color
-    height: 960px
+    height: 480px
     width: 100%
+
+
+  .el-row 
+    height: 100%
+  .el-col 
+    height: 100%
+  #header-info
+    top: 50%
+    transform: translateY(-50%)
+    padding-top: 80px
+    padding-bottom: 80px
+
+  #header-name
+
+  #header-software
+    color: $text-color-software
+  #header-ui
+    color: $text-color-ui
+  #header-mentor
+    color: $text-color-mentor
+  #header-picture
+    +size(560px, 400px)
+    top: 50%
+    transform: translateY(-50%)
+    background-image: url('~assets/images/sample-city.png')
+    background-size: cover
+    
+    #header-location
+      position: absolute
+      background-color: transparent
+      font-size: 16px
+      top: 24px
+      left: -32px
+      text-shadow: 2px 2px 4px black
+
 </style>
