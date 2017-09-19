ab68847b8b5265b6cdc498feb363fde07a82e6d6
Samuel <citysite1025@gmail.com>
2017-09-19 23:10:01 +0800

    Update Section Components

---
+++ src/components/Element/title.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div>
+    <p class="index">
+        {{index}}
+    </p>
+    <h2 class="title">
+        {{title}}
+    </h2>
+    <h3 class="description">
+        {{description}}
+    </h3>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: ["index", "title", "description"]
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+    @import url('https://fonts.googleapis.com/css?family=Playfair+Display');
+
+    .index
+        font-family: 'Playfair Display', serif
+        font-style: italic
+        font-size: 24px
+        font-weight: 300
+        letter-spacing: 4px
+    .title 
+        margin-top: 0px
+    .description
+        margin-top: -16px
+        max-width: 360px
+
+
+</style>
--- src/components/app.vue
+++ src/components/app.vue
@@ -8,7 +8,7 @@
         <div class="rope"></div>
         <div class="rope"></div>
     </div>
-    <section class="section_about">
+    <section class="section-about">
         <el-row>
             <el-col :span="10" :offset="2">
                 <div id="header-info">
@@ -25,8 +25,8 @@
                         Mentor <span>in Taiwan</span>
                     </h3>
                 </div>
-        </el-col>
-            <el-col :span="12" id="apps">
+            </el-col>
+            <el-col :span="12">
                 <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="header-picture-container">
                     <div id="header-picture">
                         <h3 id="header-location">
@@ -37,25 +37,90 @@
             </el-col>
         </el-row>
     </section>
-    <section class="section_slogan">
+    <section class="section-slogan">
         <el-row>
             <el-col :span="10" :offset="2">
                 <h1 id="slogan-1">CREATE<br> MORE</h1>
                 <h1 id="slogan-2">EVERY<br> DAY</h1>
             </el-col>
         </el-row>  
-
     </section>>
-    <section class="section_slogan">
-      
-
+    <section class="section-designer">
+        <el-row>
+            <el-col :span="10" :offset="4">
+                <titleContainer index="01" title="Designer" description="User Interace, Graphic Design,
+Commercial Design."></titleContainer>
+            </el-col>
+            <el-col :span="10">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-1-container">
+                    <div class="work work-1">
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="10" :offset="3">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-2-container">
+                    <div class="work work-2">
+                    </div>
+                </div>
+            </el-col>
+            <el-col :span="11">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-3-container">
+                    <div class="work work-3">
+                    </div>
+                </div>
+            </el-col>
+        </el-row>    
+    </section>
+    <section class="section-motto">
+        <el-row>
+            <el-col :span="12" :offset="6">
+                <h1 id="motto">It's all about<br>the way<br>you design.</h1>
+            </el-col>
+        </el-row>  
     </section>>
+    <section class="section-developer">
+        <el-row>
+            <el-col :span="11" :offset="3">
+                <titleContainer index="02" title="Developer" description="Swift, ObjectiveC, Html, CSS, Sass, Javascript, JQuery, Vue.js."></titleContainer>
+            </el-col>
+            <el-col :span="10">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-4-container">
+                    <div class="work work-4">
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="11" :offset="3">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-5-container">
+                    <div class="work work-5">
+                    </div>
+                </div>
+            </el-col>
+            <el-col :span="10">
+                <div data-tilt data-tilt-perspective="2500" data-tilt-transition="true" id="work-6-container">
+                    <div class="work work-6">
+                    </div>
+                </div>
+            </el-col>
+        </el-row>    
+    </section>
+    <section class="section-educator">
+        <el-row>
+            <el-col :span="10" :offset="4">
+                <titleContainer index="03" title="Educator" description="An educator in Hahow, NTU, NTUST, NCCU with more than 500 students in Taiwan."></titleContainer>
+            </el-col>
+        </el-row>
+    </section>
 
   </div>
 </template>
 
 <script>
   import HeaderBar from './Element/header.vue';
+  import TitleContainer from './Element/title.vue';
   import tilt from 'tilt.js';
 
   export default {
@@ -69,12 +134,37 @@
         glare: true,
         maxGlare: .5
       });
+      $('#work-1-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
+      $('#work-2-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
+      $('#work-3-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
+      $('#work-4-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
+      $('#work-5-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
+      $('#work-6-container').tilt({
+        glare: true,
+        maxGlare: .5
+      });
     },
     methods: {
 
     },
     components: {
       HeaderBar,
+      TitleContainer
     }
   }
 </script>
@@ -82,11 +172,14 @@
 <style lang="sass">
     @import '~styles/main.sass'
     
-    .section_about
+    .section-about
         background-color: $default-background-color
         margin-top: 80px
         height: 480px
         width: 100%
+    .section-slogan
+        margin-top: 40px
+        margin-bottom: 40px
 
 
     .el-row 
@@ -107,11 +200,11 @@
         width: 100%
     .rope
         position: absolute
-        background-color: white
+        background-color: #222222
         width: 1px
         height: 100%
-        opacity: 0.1
-        z-index: 1
+        opacity: 0.5
+        z-index: 0
 
     .ropes .rope:nth-child(1)
         margin-left: calc(100%/6)
@@ -163,4 +256,62 @@
         text-shadow: 6px 6px 4px black
         letter-spacing: 12px
         line-height: 96px
+
+
+
+
+    .section-designer
+        #work-1-container
+            +size(250px, 310px)
+            .work-1
+                background-image: url('~assets/images/sample-city.png')
+
+        #work-2-container
+            +size(340px, 470px)
+            .work-2
+                background-image: url('~assets/images/sample-city.png')
+
+        #work-3-container
+            +size(340px, 450px)
+            margin-top: 160px
+            .work-3
+                background-image: url('~assets/images/sample-city.png')
+
+        .work
+            height: 100%
+            width: 100%
+            background-size: cover
+            z-index: 3
+
+
+    #motto
+        margin-left: auto
+        margin-right: auto
+        letter-spacing: 2px
+        line-height: 96px
+
+    .section-developer
+        #work-4-container
+            +size(400px, 440px)
+            .work-4
+                background-image: url('~assets/images/sample-city.png')
+
+        #work-5-container
+            margin-top: -120px
+            +size(400px, 440px)
+            .work-5
+                background-image: url('~assets/images/sample-city.png')
+
+        #work-6-container
+            +size(400px, 440px)
+            margin-top: 160px
+            .work-6
+                background-image: url('~assets/images/sample-city.png')
+
+        .work
+            height: 100%
+            width: 100%
+            background-size: cover
+            z-index: 3
+
 </style>
